5dfe1e4cf20b5aa8ea80152ae04ad164e3f93be5
Jesse <jesse.wolcott@mac.com>
2022-05-23 17:47:34 -0400

    First code commit
    This might be all I can commit, don't have access to a Tanium workspace anymore!

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # tanium-powershell
+
+Tanium is a really great tool for keeping your systems up to date, and even though I usually don't like third party tools when the commonly prescribed method is first party (like the ole standby, SCCM), if you're slotting into a situation that already has a tool like Tanium, best to make the most use of the tools you have. Love the one you're with, as they say. 
+
+This script fills in the gaps left by the Tanium documentation and example code. Whenever a system says "You can query our system using natural language!" that typically means that you have to learn how the system wants to be asked, and Tanium is no exception. I'd recommend limiting your scope and really nailing down your question before automating the process using this script. 
+
+Additionally, there is a way to tokenize your login, and that is probably the best thing you could do. Plain-text passwords are usually a really bad idea.
+
+## Pre-Requisites
+
+The Tanium powershell module that I used for this script is included here, in its entirety, and referenced in a relative way. 
+
+## Usage
+
+See the comments for actual script setup, but generally, I'd use a scheduled task to run this as a Service Account that has been granted access to whatever machines you want it to have access to, inside Tanium. This script, in the example, asks Tanium to give us back all servers that have been up for more than 33 days. It doesn't exactly do that, it just doesn't put the days up (it puts [no results]), so that is handled in here too.
+
+First, the script clears out the results directory. Set this up carefully, we just nuke and pave. Next, it initiates a web session with the tanium server, and asks the question, and puts all the results in an array. After that, it filters out all the [no results] results from '_Uptime (integer)'. You can change this if you want to filter out some or none of the results. After that, the entire data set is exported to Excel with Filter and fit turned on and named for the date. Next, the results are carved up and reports are generated for each of the sites that are listed in "AD Query - Computer Site" and made into their own excel sheets. Next, the results folder is checked for empty reports, which are then deleted. 
+
+
+
---
+++ server-patching-question.ps1
@@ -0,0 +1,88 @@
+﻿
+Install-Module -Name ImportExcel 
+Import-Module $PSScriptRoot\TanRest-master-latest\TanRest-master\TanRest
+
+# What AD Query - Computer Site Name are we looking for?
+$adquerycomputersites = @(
+"CORP"
+"Baltimore"
+"Baton Rouge"
+"DEV"
+)
+
+# Username and password in Clear Text 
+$testUserName = 'TestTaniumUser'
+$TestPassword = 'UseTokenizedLoginsPlease'
+$TestCredential = [PSCredential]::new($TestUserName,(convertTo-SecureString $TestPassword -AsPlainText -force))
+
+# Where should we save results? Default is "Results" in PSScriptRoot
+
+$ResultsFolder = "$PSScriptRoot\Results"
+
+# Tanium Server URL
+
+$TaniumServer = 'https://testtanium.url.google.training'
+
+# Tanium Question
+$Question = 'Get Computer Name and AD Query - Computer Site Name and Uptime and "_Uptime (integer)"?ignoreCase=0 > 33" from all machines with Windows OS Type contains server'
+
+# Folder testing
+if (Test-Path -Path $ResultsFolder) {
+    Write-Output "Results folder found."
+} else {
+    Write-Output "No Results folder found, creating now...."
+    New-Item -Path $ResultsFolder -ItemType Directory
+}
+
+
+
+#Remove old files
+Write-Output "Removing old files."
+Remove-Item $ResultsFolder\* -Include *.xlsx
+
+## Tanium Section - Asking for patch schedule and uptime for all servers
+Write-Output "Initiating Tanium Web Session"
+$WebSession = New-TaniumWebSession -ServerURI $TaniumServer -credential $TestCredential
+
+$Parsed = New-TaniumCoreParseQuestion -data @{ text = $Question } 
+
+$taniumarray = @{ query_text = $Parsed[0].question_text } | New-TaniumCoreQuestions |  # Ask the question
+wait-TaniumCoreQuestionResultInfo -Timeout 120 |   # Wait for all machines to respond
+Get-TaniumCoreQuestionResult | #  Get the results
+Format-TaniumCoreQuestionResult # Format the results into a Powershell Friendly format
+
+### end tanium ask
+
+## Filter out [no results] because tanium is goofy
+
+$taniumarray.where{$_.'_Uptime (integer)' -ne '[no results]'} | ConvertTo-Excel -AutoFilter -AutoFit -FreezeTopRow -ExcelWorkSheetName "Report Generated $DateTime" -FilePath "$ResultsFolder\Servers_MissedReboots_AllServers.xlsx"
+
+
+foreach ($adquerycomputersite in $adquerycomputersites){
+
+$DateTime = Get-Date -format "MMddyyyy-HHmmss"
+
+$taniumarray.where{($_.'AD Query - Computer Site Name' -like "$adquerycomputersite*") -and $_.'_Uptime (integer)' -ne '[no results]'} |  ConvertTo-Excel -AutoFilter -AutoFit -FreezeTopRow -ExcelWorkSheetName "$adquerycomputersite$DateTime" -FilePath "$ResultsFolder\Servers_MissedReboots_$ADQueryComputersite.xlsx"
+
+}
+#Clean up
+
+$Filestocheck = @(Get-ChildItem "$ResultsFolder\" -Filter *.xlsx)
+
+Foreach ($file in $Filestocheck) {
+
+    $RowCount = ((Get-ExcelFileSummary -Path "$ResultsFolder\$File" -ErrorAction SilentlyContinue).rows)
+    
+    If ($null -eq $RowCount) {
+
+        Remove-Item "$ResultsFolder\$File"
+        Write-Output "Removed $file"
+    
+     }
+
+    Else {
+
+        Write-Output "$file has $rowcount rows"
+    }
+
+}
